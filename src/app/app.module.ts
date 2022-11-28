3300acc6e30ffabd135ccffae66cda8780cdae2a
ArseniyS8 <arsensurkov22@gmail.com>
2022-11-28 16:07:39 +0600

    added njrx(models, reducers, services, effects, action)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {RouterOutlet} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app.routing.module";
+import {environment} from "../environments/environment";
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {FilmModule} from "./imdb-films/film.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        NgbModule,
-        RouterOutlet,
-        FormsModule,
-        HttpClientModule,
+  imports: [
+    BrowserModule,
+    NgbModule,
+    RouterOutlet,
+    FormsModule,
+    HttpClientModule,
+    FilmModule,
+    AppRoutingModule,
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
 
-    ],
+  ],
   providers: [],
   exports: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
